3edb86e2c351b6f10fa47dc8ed95d6bcabc97a51
New <New@KOMPUTER>
2026-02-21 18:40:33 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,29 @@
+﻿// exercise2_buggy.cpp
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    int* x, * y, * z, d;
+
+    cout << "3 number, please" << endl;
+    cin >> *x >> *y >> *z;  // ОШИБКА: указатели не инициализированы!
+
+    if (x > y)  // ОШИБКА: сравниваем адреса, а не значения
+    {
+        d = *x; x = y; *y = d;
+    }
+    if (x > z)
+    {
+        d = *z; z = x; *x = d;
+    }
+    if (y > z)
+    {
+        d = *y; y = z; *z = d;
+    }
+
+    cout << "x=" << x << " y=" << y << " z=" << z << endl;
+
+    return 0;
+}
